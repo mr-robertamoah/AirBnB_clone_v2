7ec0cf8866d7066005c817565c1d6858bb3fc603
Robert Amoah <mr_robertamoah@yahoo.com>
2023-12-30 07:35:02 +0000

    done with task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,6 +5,8 @@ script that starts a Flask web application
 web application must be listening on 0.0.0.0, port 5000
 route /: display 'Hello HBNB!'
 route /hbnb: display 'HBNB'
+route /c/<text>: display C followed by the value of the text 
+    variable (replace underscore _ symbols with a space )
 """
 
 
@@ -24,6 +26,13 @@ def index():
 def hbnb():
     """ returns a string """
     return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ returns a string """
+    text = text.replace("_", " ")
+    return f"C {text}"
 
 
 if __name__ == "__main__":
