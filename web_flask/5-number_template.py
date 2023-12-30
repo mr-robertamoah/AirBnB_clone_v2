3109f60495f7fd2a5a983c1dd3e8a64854597b07
Robert Amoah <mr_robertamoah@yahoo.com>
2023-12-30 08:01:16 +0000

    done with task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+"""
+script that starts a Flask web application
+web application must be listening on 0.0.0.0, port 5000
+route /: display 'Hello HBNB!'
+route /hbnb: display 'HBNB'
+route /c/<text>: display C followed by the value of the text
+    variable (replace underscore _ symbols with a space)
+route /python/<text>: display Python, followed by the value of the
+    text variable (replace underscore _ symbols with a space)
+route /number/<n>: display n is a number only if n is an integer
+route /number_template/<n>: display a HTML page only if n is an integer
+"""
+
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ returns a string """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ returns a string """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ returns a string """
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """ returns a string """
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def num(n):
+    """ returns a number """
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numberTemplate(n):
+    """ returns a number html file """
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0")
