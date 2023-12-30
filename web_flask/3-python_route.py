8a1ee8c44d288ada736aecddadafd54e8b3806fe
Robert Amoah <mr_robertamoah@yahoo.com>
2023-12-30 07:45:45 +0000

    done with task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,7 +6,9 @@ web application must be listening on 0.0.0.0, port 5000
 route /: display 'Hello HBNB!'
 route /hbnb: display 'HBNB'
 route /c/<text>: display C followed by the value of the text
-    variable (replace underscore _ symbols with a space )
+    variable (replace underscore _ symbols with a space)
+route /python/<text>: display Python, followed by the value of the
+    text variable (replace underscore _ symbols with a space)
 """
 
 
@@ -30,6 +32,14 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """ returns a string """
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
     """ returns a string """
     text = text.replace("_", " ")
     return f"C {text}"
